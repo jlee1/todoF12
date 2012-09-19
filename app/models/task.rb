0e259723e22128e23a87ffa87878ca3b4b83cfb2
Christopher Slade <crslade@gmail.com>
2012-09-19 11:02:08 -1000

    Modifications added

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ActiveRecord::Base
   attr_accessible :due_date, :finished, :name
+
+  #validates :name,  :presence => true, :format => {:with => /\A[A-Z]*\z/, :message => "Needs to be all caps"}
+validates :name,   :length => {:within => 3..15}
+
+
 end
